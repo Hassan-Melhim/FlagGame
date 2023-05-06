c82f1d597175917bfceb7d9368756db56d7a1098
Hassan A Melhim <hassanmelhim2@gmail.com>
2023-05-06 22:45:36 +0300

    final commit

---
+++ app/src/main/java/edu/birzeit/myapplication/MainActivity.java
@@ -0,0 +1,146 @@
+package edu.birzeit.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView flag;
+    Button btn1,btn2,btn3,btn4;
+    TextView scoreFeild;
+    private static final long delayAmount = 1000;
+    int score;
+    ArrayList <Question> list = new ArrayList();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initializing variables
+        flag = (ImageView) findViewById(R.id.Flag);
+        btn1 = (Button) findViewById(R.id.Button1);
+        btn2 = (Button) findViewById(R.id.Button2);
+        btn3 = (Button) findViewById(R.id.Button3);
+        btn4 = (Button) findViewById(R.id.Button4);
+        scoreFeild = (TextView) findViewById(R.id.Score);
+        score = 0;
+
+        //Mock-up data
+        list.add(new Question(R.drawable.andorra,"Angola", "Algeria",
+                "Andorra", "Antigua", "Andorra"));
+        list.add(new Question(R.drawable.antigua_barbuda, "Barbados", "Bahamas",
+                "Andorra", "Antigua&barbuda", "Antigua&barbuda"));
+        list.add(new Question(R.drawable.eritrea, "Ethiopia", "Eritrea",
+                "Djibouti", "Congo", "Eritrea"));
+        list.add(new Question(R.drawable.montenegro, "Montenegro", "Morocco",
+                "Macedonia", "Mozambique", "Montenegro"));
+        list.add(new Question(R.drawable.mozambique, "Morocco", "Mozambique",
+                "Macedonia", "Montenegro", "Mozambique"));
+
+        runTest();
+    }
+    public void runTest (){
+        //choosing flag at random
+       int random = (int)(Math.random() * this.list.size());
+
+       //presenting score
+        scoreFeild.setText("Score: " + score);
+
+       //presenting the randomly generated flag
+       flag.setImageResource(list.get(random).getFlagID());
+       btn1.setText(list.get(random).getOption1());
+       btn2.setText(list.get(random).getOption2());
+       btn3.setText(list.get(random).getOption3());
+       btn4.setText(list.get(random).getOption4());
+
+       //setting up action for the button click
+       btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(btn1, list.get(random));
+            }
+        });
+
+       btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(btn2, list.get(random));
+            }
+        });
+
+       btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(btn3, list.get(random));
+            }
+        });
+
+       btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(btn4, list.get(random));
+            }
+        });
+    }
+
+    public void checkAnswer(Button btn, Question question){
+        btn1.setEnabled(false);
+        btn2.setEnabled(false);
+        btn3.setEnabled(false);
+        btn4.setEnabled(false);
+
+        if(btn.getText().toString().equals(question.getCorrectAns())){
+            btn.setBackgroundColor(Color.BLUE);
+            this.score += 100;
+        }
+
+        else{
+            btn.setBackgroundColor(Color.RED);
+            this.score -=50;
+        }
+
+
+        //Delay before going to the next question
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //reset button colours
+                btn1.setBackgroundColor(getResources().getColor(R.color.AcademicGreen));
+                btn2.setBackgroundColor(getResources().getColor(R.color.AcademicGreen));
+                btn3.setBackgroundColor(getResources().getColor(R.color.AcademicGreen));
+                btn4.setBackgroundColor(getResources().getColor(R.color.AcademicGreen));
+
+                if(score >= 1000){//win condition reached
+                    scoreFeild.setText("!!!!!Congrats!!!!!");
+                    flag.setImageResource(R.drawable.win);
+                    btn1.setText("Wooooooo");
+                    btn2.setText("ooooooW");
+                    btn3.setText("Yaaaaaa");
+                    btn4.setText("aaaaaaaY");
+                }
+                else{
+                    //enable button clicks again
+                    btn1.setEnabled(true);
+                    btn2.setEnabled(true);
+                    btn3.setEnabled(true);
+                    btn4.setEnabled(true);
+
+                    runTest();
+                }
+            }
+        }, delayAmount);
+    }
+}
---
+++ app/src/main/java/edu/birzeit/myapplication/Question.java
@@ -0,0 +1,72 @@
+package edu.birzeit.myapplication;
+
+import android.widget.ImageView;
+
+public class Question {
+    private int flagID;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+
+    private String correctAns;
+
+
+    public Question(int id, String opt1,
+                    String opt2, String opt3, String opt4, String Ans){
+        this.flagID = id;
+        this.option1 = opt1;
+        this.option2 = opt2;
+        this.option3 = opt3;
+        this.option4 = opt4;
+        this.correctAns = Ans;
+    }
+
+    public int getFlagID() {
+        return flagID;
+    }
+
+    public void setFlagID(int flag) {
+        this.flagID = flag;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public void setOption1(String option1) {
+        this.option1 = option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public void setOption2(String option2) {
+        this.option2 = option2;
+    }
+
+    public String getOption3() {
+        return option3;
+    }
+
+    public void setOption3(String option3) {
+        this.option3 = option3;
+    }
+
+    public String getOption4() {
+        return option4;
+    }
+
+    public void setOption4(String option4) {
+        this.option4 = option4;
+    }
+
+    public String getCorrectAns() {
+        return correctAns;
+    }
+
+    public void setCorrectAns(String correctAns) {
+        this.correctAns = correctAns;
+    }
+}
